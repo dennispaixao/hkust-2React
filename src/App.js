1ebacf4e1d8443fd743e87d44f9f64ac684c48c0
Dennis <dennispaixaoads@gmail.com>
2021-06-03 16:38:50 -0300

    react-redux-form

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import './App.css';
 import { Component } from 'react';
 import Main from './components/MainComponent';
 import {BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 class App extends Component {
 
   render() {
     return (
+      <Provider store={store}>
       <BrowserRouter>
-      <div className="App">
-        <Main />
-      </div>
+        <div className="App">
+          <Main />
+        </div>
       </BrowserRouter>
+    </Provider>
     );  
   }
 }
